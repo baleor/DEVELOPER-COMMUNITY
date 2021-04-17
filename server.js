4f045d8d538fc3863ba68ccae70cf8d61724274c
baleor <raoanshul294@.com>
2021-04-17 21:42:39 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -24,4 +24,4 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 const port = process.env.PORT || 5000 ;
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
