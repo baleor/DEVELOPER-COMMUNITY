9bcacea42df50eca59a845ad8c5bc63e251d1cbb
baleor <raoanshul294@.com>
2021-04-17 22:19:16 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const  express = require('express')
 const connectDB =require('./config/db')
 const path = require('path')
 const app = express()
-var cors = require('cors')
+var cors = require('cors') 
 
 connectDB();
 
