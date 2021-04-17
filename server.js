1bb25ebe6f2333a5d70394a90080d7239a5fd184
baleor <raoanshul294@.com>
2021-04-17 22:03:50 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
 
     app.get('*',(req, res) => {
+        console.log(path);
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     })
 }
